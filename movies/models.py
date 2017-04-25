e9de254bae0fb4f961ea8899da035bcc5a3d4b5c
Joanna Crystal <jcs771@nyu.edu>
2017-04-25 14:33:39 -0400

    iMax project

---
+++ movies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+#name = models.CharField(max_length=200
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    phone_number = models.CharField(max_length=13, blank=True)
+    event_name = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return User.first_name + " " + User.last_name
+
+
---
+++ movies/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, {'template_name':'movies/login.html'}, name='login'),
+]
